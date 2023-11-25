c2e16fdd900c0b0bb6ef649fb24c7b2a1e35b7c3
xh2342 <xfh2342@gmail.com>
2023-11-24 22:04:57 -0500

    create the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-export default function App() {
+import Header from "./components/Header";
+
+function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
@@ -17,3 +20,5 @@ export default function App() {
     </BrowserRouter>
   );
 }
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6 mx-auto p-3">
+        <Link to="/">
+          {" "}
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Home</span>
+            <span className="text-slate-700">Hive</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-xl flex items-center">
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent focus:outline-none w-18 sm: w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="sm:inline text-slate-700 hover:underline">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;

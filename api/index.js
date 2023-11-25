a2b903261fb28109c6eeba97f4c782dfbd0a8225
xh2342 <xfh2342@gmail.com>
2023-11-24 22:49:02 -0500

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connectioned to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;

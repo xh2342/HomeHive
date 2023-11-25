a194f5e0860f748ca47d2d740437fe502615d789
xh2342 <xfh2342@gmail.com>
2023-11-25 03:33:48 -0500

    update background color

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function SignUp() {
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold">Sign Up</h1>
+    <div className="p-3 max-w-lg mx-auto ">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form className="flex flex-col gap-4">
         <input
           type="text"

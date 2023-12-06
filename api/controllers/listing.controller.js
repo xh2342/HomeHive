d41464cd2281fae8345d1df3eef4d7fdd6e3ce72
xh2342 <xfh2342@gmail.com>
2023-12-05 22:26:09 -0500

    complete update listing route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -32,3 +32,26 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+
+  if (listing.userRef !== req.user.id) {
+    return next(errorHandler(401, "you can only update your own listing!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ import {
   test,
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/", test);
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
